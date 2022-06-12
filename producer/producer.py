d00d8d28cc507424674a50e2c48298aa77a89cbe
avison9 <avisonlivingstone@gmail.com>
2022-06-12 11:13:14 +0100

    Committing producer file

---
+++ producer/producer.py
@@ -0,0 +1,24 @@
+from time import sleep
+from json import dumps
+from kafka import KafkaProducer
+
+
+producer = KafkaProducer(
+	bootstrap_servers=['kafka:9092'],
+	value_serializer=lambda x:dumps(x).encode('utf-8'),
+	api_version=(5, 5, 0)
+	)
+producing_topic = 'input-topic'
+wrong_data = [
+	{"myKey": 1, "myTimestamp": "2022-03-01T09:11:04+01:00"},
+	{"myKey": 2, "myTimestamp": "2022-03-01T09:12:08+01:00"},
+	{"myKey": 3, "myTimestamp": "2022-03-01T09:13:12+01:00"},
+	{"myKey": 4, "myTimestamp": ""},
+	{"myKey": 5, "myTimestamp": "2022-03-01T09:14:05+01:00"},
+]
+
+for idx, msg in enumerate(wrong_data):
+	print(f'sending message {idx}')
+	producer.send(producing_topic,value=msg)
+	sleep(2)
+
